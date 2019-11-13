a647aa09a3c63031d36d359b8f3ed76523d7e258
Raphael Panic <Raphael.Panic@catalysts.cc>
2019-11-13 11:30:25 +0100

    Implemented Moluarized Backend Architecture

---
+++ src/main/kotlin/db/Backend.kt
@@ -0,0 +1,17 @@
+package db
+
+import kotlin.reflect.KClass
+
+interface Backend {
+
+    fun keyExists(key: String) : Boolean
+
+    fun <T : Observable> load(key: String, clazz: KClass<T>) : T
+
+    fun <T : Observable> loadList(key: String, clazz: KClass<T>) : List<T>
+
+    fun <T : Observable> save(key: String, clazz: KClass<T>, obj: T)
+
+    fun <T : Observable> saveList(key: String, clazz: KClass<T>, obj: List<T>)
+
+}
