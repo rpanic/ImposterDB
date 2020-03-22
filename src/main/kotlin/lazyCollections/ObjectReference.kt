07f2bda52ed8779e6dde52078972c2529749e9c1
rpanic <raphael.panic@gmx.at>
2020-03-22 17:39:42 +0100

    Added ObjectReference for lazy Lists

---
+++ src/main/kotlin/lazyCollections/ObjectReference.kt
@@ -0,0 +1,44 @@
+package lazyCollections
+
+typealias OnLoadListener<T> = (T) -> Unit
+
+class ObjectReference <T : Indexable> {
+
+    val pk : Any
+    val load : ((Any) -> T)?
+
+    constructor(pk: Any, load: (Any) -> T){
+        this.pk = pk
+        this.load = load
+    }
+
+    constructor (obj: T) {
+        this.pk = obj.key()
+        this.load = null
+    }
+
+    var obj: T? = null
+    var loaded = false
+
+    fun getObject() : T{
+        if(!loaded) {
+            obj = load!!(pk)
+            onLoadList.forEach { it(obj!!) }
+            loaded = true
+        }
+        return obj!!
+    }
+
+    fun setObject(t: T){
+        obj = t
+        onLoadList.forEach { it(obj!!) }
+        loaded = true
+    }
+
+    private val onLoadList = mutableListOf<OnLoadListener<T>>()
+
+    fun onLoad(onLoad: OnLoadListener<T>){
+        onLoadList += onLoad
+    }
+
+}
