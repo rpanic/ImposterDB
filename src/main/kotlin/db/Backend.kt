9966a2fcdf03b5681ad05ba53c03b0c63405d83f
rpanic <rpanic.rbs1933@gmx.at>
2020-04-02 12:52:23 +0200

    Removed findDelegationProperties from Backend

--- src/main/kotlin/db/Backend.kt
+++ src/main/kotlin/db/Backend.kt
@@ -25,27 +25,6 @@ interface Backend {
     fun <T : Observable, K> delete(key: String, clazz: KClass<T>, pk: K)
 
     fun <T : Observable> insert(key: String, clazz: KClass<T>, obj: T)
-
-    fun <T : Observable, R : Any?, DELEGATE : Any> isPropertyDelegating(clazz: KClass<T>, prop: KProperty1<T, R>, delegatingTo: KClass<DELEGATE>): Boolean {
-        val javaField = prop.javaField
-        return if (javaField != null && delegatingTo.java.isAssignableFrom(javaField.type)) {
-            javaField.isAccessible = true // is private, have to open that up
-            @Suppress("UNCHECKED_CAST")
-            val delegateInstance = javaField.type == delegatingTo.java
-            delegateInstance
-        } else {
-            false
-        }
-    }
-
-    fun <T : Observable, DELEGATE : Any> findDelegatingProperties(clazz: KClass<T>, delegatingTo: KClass<DELEGATE>): List<KProperty1<T, DELEGATE>> {
-        return clazz.declaredMemberProperties.map { prop ->
-            if(isPropertyDelegating(clazz, prop, delegatingTo)){
-                prop as KProperty1<T, DELEGATE>
-            }else
-                null
-        }.filterNotNull()
-    }
 }
 
 abstract class DBBackend : DBAwareObject(), Backend {
