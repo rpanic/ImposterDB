46e720df04253bf50636ea37b53c3c7d9a911ec2
rpanic <rpanic.rbs1933@gmx.at>
2020-04-20 15:16:08 +0200

    Added Zeko Dependency

--- src/main/kotlin/example/Try.kt
+++ src/main/kotlin/example/Try.kt
@@ -1,12 +1,25 @@
 package example
 
 import aNewCollections.invoke
+import io.zeko.db.sql.Insert
+import io.zeko.db.sql.Query
+import io.zeko.db.sql.Update
+import io.zeko.model.Entity
 import observable.Observable
 
 fun main() {
 
-    val x = ""()
-    println()
+    val entity = GenericEntity("uuid" to "Hello", "name" to "wtf", "testint" to 0)
+
+    Update(entity).toSql().print()
+
+    Insert(entity).toSql().print()
+
+}
+
+class GenericEntity : Entity {
+    constructor(map: Map<String, Any?>) : super(map)
+    constructor(vararg props: Pair<String, Any?>) : super(*props)
 
 }
 
